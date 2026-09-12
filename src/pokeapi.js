7f8ecb2d00694739ac8ad150c7357e09e92197f2
scaulfield7 <scaulfield@live.ie>
2026-09-12 20:41:50 +0100

    Remove unused options

--- src/pokeapi.js
+++ src/pokeapi.js
@@ -1,13 +1,5 @@
 import Pokedex from 'pokedex-promise-v2';
 
-var options = {
-  protocol: 'https',
-  hostName: 'localhost:443',
-  versionPath: '/api/v2/',
-  cacheLimit: 100 * 1000, // 100s
-  timeout: 5 * 1000 // 5s
-}
-
 const P = new Pokedex();
 
   P.getPokemonByName(1, function(response, error) { // with callback
