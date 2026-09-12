91429c2a84a8f23c000533b5d4c34d3fdce83175
scaulfield7 <scaulfield@live.ie>
2026-09-13 00:07:59 +0100

    Revert change to name variable

--- src/pokemonlist.js
+++ src/pokemonlist.js
@@ -3,6 +3,7 @@ import data from './data/data.json';
 
 let url = "";
 let pokemonId = "";
+let name = "";
 
 class PokemonList extends Component {
   render() {
@@ -16,7 +17,7 @@ class PokemonList extends Component {
         
         {data.results.map((postDetail, index)=>{
           url = postDetail.url;
-          PokemonList.name = postDetail.name;
+          name = postDetail.name;
           pokemonId = url.substr(34).slice(0, -1);
 
           return <h4><a href={'pokemonlist/pokemon/'+pokemonId}>{postDetail.name}</a></h4>
